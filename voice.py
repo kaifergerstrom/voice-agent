1bb200e876c139a67fc0cdbe72688dc6b5dea1ad
Kai Fergerstrom <kaifergerstrom@gmail.com>
2020-03-23 16:08:15 -0400

    Added command listener

--- voice.py
+++ voice.py
@@ -11,19 +11,30 @@ class Voice:
 
 class Listener:
 
+    name = "Alexa"
+    command = ""
+
     def __init__(self):
         self.r = sr.Recognizer()
 
-voice = Voice()
+    def listen_for_command(self):
+        with sr.Microphone() as source:     # mention source it will be either Microphone or audio files.
+            print("Speak Anything :")
+            while True:
+                audio = self.r.listen(source)        # listen to the source
+                try:
+                    text = self.r.recognize_google(audio)    # use recognizer to convert our audio into text part.
+                    if self.name.lower() in text.lower():
+                        fullmsg = text
+                        self.command = text.lower().split(self.name.lower())[1].strip()
+                        return command
+                except:
+                    print("Sorry could not recognize your voice")    # In case of voice not recognized 
+        return ""
+        
+Listener = Listener()
+print(Listener.listen_for_command())
+#voice = Voice()
 
-voice.say("Hello, kai")
+#voice.say("Hello, kai")
 
-r = sr.Recognizer()
-
-with sr.Microphone() as source:
-    # read the audio data from the default microphone
-    audio_data = r.record(source, duration=5)
-    print("Recognizing...")
-    # convert speech to text
-    text = r.recognize_google(audio_data)
-    print(text)

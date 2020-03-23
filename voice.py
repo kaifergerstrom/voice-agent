7997fef35755c3cb885001d3fa7450ae6cd54ef1
Kai Fergerstrom <kaifergerstrom@gmail.com>
2020-03-23 15:41:01 -0400

    Basic voice agent

---
+++ voice.py
@@ -0,0 +1,12 @@
+import pyttsx3
+
+class Voice:
+    def __init__(self):
+        self.engine = pyttsx3.init()
+
+    def say(self, msg):
+        self.engine.say(msg)
+        self.engine.runAndWait()
+
+voice = Voice()
+voice.say("Hello, kai")

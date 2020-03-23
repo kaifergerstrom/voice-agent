6886c294c47fcf957ebaa71b029319613ad9db37
Kai Fergerstrom <kaifergerstrom@gmail.com>
2020-03-23 15:45:44 -0400

    Imported speech recognizer

--- voice.py
+++ voice.py
@@ -1,4 +1,5 @@
 import pyttsx3
+import speech_recognition as sr
 
 class Voice:
     def __init__(self):
@@ -8,5 +9,21 @@ class Voice:
         self.engine.say(msg)
         self.engine.runAndWait()
 
+class Listener:
+
+    def __init__(self):
+        self.r = sr.Recognizer()
+
 voice = Voice()
+
 voice.say("Hello, kai")
+
+r = sr.Recognizer()
+
+with sr.Microphone() as source:
+    # read the audio data from the default microphone
+    audio_data = r.record(source, duration=5)
+    print("Recognizing...")
+    # convert speech to text
+    text = r.recognize_google(audio_data)
+    print(text)

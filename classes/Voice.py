8d7b341ae602de8efd450e643f91a52e9cf794a4
Kai Fergerstrom <kaifergerstrom@gmail.com>
2020-03-28 21:01:17 -0400

    Made voice command end task currently talking, only one proccess at a time!

--- classes/Voice.py
+++ classes/Voice.py
@@ -1,17 +1,23 @@
 from subprocess import Popen
-import pyttsx3, os
+import pyttsx3, os, time
 
 class Voice:  # Threaded text to speech command
 
     def __init__(self):
         
         self.engine = pyttsx3.init()  # Initialize text to speech library
-        voices = self.engine.getProperty('voices')
+
+        # Adjust pyttsx3 bot voice
+        voices = self.engine.getProperty('voices') 
         self.engine.setProperty('voice', voices[1].id)
+
+        self.p = None  # Start with empty thread
 
     def say(self, msg):
         dir_path = os.path.dirname(os.path.dirname( __file__ )) + '\\speak.py'  # Get relative path to speak.py depending on where script is executed
-        Popen(["python", dir_path, msg])  # Run threaded background command
+        if self.p:  # If in the middle of talking, end it
+            self.p.kill()
+        self.p = Popen(["python", dir_path, msg])  # Run threaded background command
 
 
 if (__name__ == "__main__"):
